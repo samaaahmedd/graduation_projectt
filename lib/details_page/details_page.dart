03e76c432f00e2e5a6c8d12757e37a686d6825d7
Eng Salma <samaa>
2023-05-05 17:13:52 +0300

    create new page

--- lib/details_page/details_page.dart
+++ lib/details_page/details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../common/custom_widgets/custom_button.dart';
+
 class DetailsPage extends StatefulWidget {
   const DetailsPage({Key? key}) : super(key: key);
 
@@ -11,33 +13,93 @@ class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Color(0xff5661f1),
+      backgroundColor: const Color(0xff5661f1),
       appBar: AppBar(
         automaticallyImplyLeading: true,
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 200,
-              width: 200,
-              decoration: const BoxDecoration(
-                  color: Colors.grey,
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          "https://thumbs.dreamstime.com/b/surprised-female-person-confused-isolated-surprised-female-person-confused-isolated-168304856.jpg"),
-                      fit: BoxFit.cover)),
-            ),
-            Text("Sama , female"),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
 
+            children: [
+              Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://thumbs.dreamstime.com/b/surprised-female-person-confused-isolated-surprised-female-person-confused-isolated-168304856.jpg"),
+                          fit: BoxFit.cover)),
+                ),
+              ),
+              const SizedBox(height: 20,),
+              Row(
+                children: [
+                  const Text("Sama , ",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold) ),
+                  const Text("female",style: TextStyle(fontSize: 15, ) ),
 
-          ],
+                ],
+              ),
+              const SizedBox(height: 12,),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.location_on_outlined,
+                    size: 21,
+                  ),
 
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text( 'Egypt/cairo' ,style: TextStyle(fontSize: 15),),
+                ],
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.phone,
+                    size: 21,
+                  ),
+
+                  SizedBox(
+                    width: 5,
+                  ),
+
+                  Text( '01225555200' ,style: TextStyle(fontSize: 15),),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              const Divider (thickness: 1,color: Colors.white,
+              ),
+              const SizedBox(height: 5,),
+          const Text("Tourguide" ,style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+              const SizedBox(height: 5,),
+              const Divider(thickness: 1,color: Colors.white,),
+              const SizedBox(height: 12,),
+              const Text("About Me" ,style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10,),
+              const Text("Hello, my name's sama" ,style: TextStyle(fontSize: 15),),
+
+              const Divider (thickness: 1,color: Colors.white,
+              ),
+              CustomButton(
+                onPressed: () {
+
+                },
+                text: "Send Request",
+              ),
+
+            ],
+
+          ),
         ),
       ),
     );
